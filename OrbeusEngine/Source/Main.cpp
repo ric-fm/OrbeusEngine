86953b1b726220c2f9942f7bcb504d6b829cbbb5
Ricardo Franco <ricardocaena@gmail.com>
2018-06-30 22:59:48 +0200

    Test use of Vertex Array Object

--- OrbeusEngine/Source/Main.cpp
+++ OrbeusEngine/Source/Main.cpp
@@ -102,11 +102,17 @@ int main()
 		return -1;
 	}
 
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
 	float vertices[] = {
 		-0.5f, -0.5f, // left  
 		 0.5f, -0.5f, // right 
 		 0.0f,  0.5f, // top   
 	};
+
+	unsigned int vao;
+	glGenBuffers(1, &vao);
+	glBindBuffer(GL_VERTEX_ARRAY, vao);
 
 	unsigned int vbo;
 	glGenBuffers(1, &vbo);
@@ -116,10 +122,11 @@ int main()
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 	unsigned int shaderProgram = createShader("Resources/Basic-vs.shader", "Resources/Basic-fs.shader");
 
 	glUseProgram(shaderProgram);
-
 
 	while (!glfwWindowShouldClose(window))
 	{
