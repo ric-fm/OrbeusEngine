dcc661565f7b72527cc2bc827b210bee92c9ca33
Ricardo Franco <ricardocaena@gmail.com>
2018-06-30 17:53:53 +0200

    Added glfw3-3.2.1 and created a simple window

--- OrbeusEngine/Source/Main.cpp
+++ OrbeusEngine/Source/Main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 int main()
 {
-	std::cout << "Orbeus Engine" << std::endl;
-	std::cin.get();
+	GLFWwindow* window;
 
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(800, 600, "Orbeus Engine", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
