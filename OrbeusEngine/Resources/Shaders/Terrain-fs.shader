6de11656f2117c9cddda539b18532c356ba5540d
Ricardo Franco <ricardocaena@gmail.com>
2018-11-18 17:14:07 +0100

    Added lights to Terrain Rendering

--- OrbeusEngine/Resources/Shaders/Terrain-fs.shader
+++ OrbeusEngine/Resources/Shaders/Terrain-fs.shader
@@ -1,9 +1,11 @@
 #version 330
 
-out vec4 FragColor;
+#include Lighting-header.shader
 
+out vec4 FragColor;
 in vec3 Normal;
 in vec2 TexCoord;
+in vec3 WorldPos;
 
 uniform sampler2D background_texture;
 uniform sampler2D red_texture;
@@ -11,18 +13,27 @@ uniform sampler2D green_texture;
 uniform sampler2D blue_texture;
 uniform sampler2D blend_map;
 
-
-void main()
+vec3 calcMultiTextureColor(sampler2D background_texture, sampler2D red_texture, sampler2D green_texture, sampler2D blue_texture, sampler2D blend_map, vec2 texCoord, float tiling)
 {
-	vec4 blendColor = texture(blend_map, TexCoord);
+	vec4 blendColor = texture(blend_map, texCoord);
+	vec2 tiledTexCoord = texCoord * tiling;
 
-	float backTextureAmount = 1 - (blendColor.r + blendColor.g + blendColor.b);
-	vec2 tiledTexCoord = TexCoord * 40.0;
-
-	vec4 backgroundColor = texture(background_texture, tiledTexCoord) * backTextureAmount;
+	vec4 backgroundColor = texture(background_texture, tiledTexCoord) * (1 - (blendColor.r + blendColor.g + blendColor.b));
 	vec4 rColor = texture(red_texture, tiledTexCoord) * blendColor.r;
 	vec4 gColor = texture(green_texture, tiledTexCoord) * blendColor.g;
 	vec4 bColor = texture(blue_texture, tiledTexCoord) * blendColor.b;
 
-	FragColor = backgroundColor + rColor + gColor + bColor;
+	return (backgroundColor + rColor + gColor + bColor).xyz;
+}
+
+void main()
+{
+	vec3 color = calcMultiTextureColor(background_texture, red_texture, green_texture, blue_texture, blend_map, TexCoord, 100.0f);
+
+	LightResult light = calcLights(Normal, WorldPos);
+
+	vec3 ambient = light.ambient * color;
+	vec3 diffuse = light.diffuse * color;
+
+	FragColor = vec4(ambient + diffuse, 1.0f);
 }
--- OrbeusEngine/Source/Rendering/Terrain/TerrainRenderer.cpp
+++ OrbeusEngine/Source/Rendering/Terrain/TerrainRenderer.cpp
@@ -15,6 +15,8 @@
 #include "Components/Terrain.h"
 #include "Components/Camera.h"
 
+#include "Components/Light.h"
+
 TerrainRenderer::TerrainRenderer()
 {
 	shader = new Shader("Resources/Shaders/Terrain-vs.shader", "Resources/Shaders/Terrain-fs.shader");
@@ -38,12 +40,55 @@ TerrainRenderer::~TerrainRenderer()
 void TerrainRenderer::render()
 {
 	std::vector<Terrain*> terrains = World::getInstance().getComponents<Terrain>();
+	std::vector<DirectionalLight*> directionalLights = World::getInstance().getComponents<DirectionalLight>();
+	std::vector<PointLight*> pointLights = World::getInstance().getComponents<PointLight>();
+	std::vector<SpotLight*> spotLights = World::getInstance().getComponents<SpotLight>();
 
 	if (terrains.size() > 0)
 	{
 		shader->bind();
 		shader->SetMatrix("view", World::getInstance().getActiveCamera()->getViewMatrix());
 		shader->SetMatrix("projection", World::getInstance().getActiveCamera()->getProjectionMatrix());
+		shader->SetFloat3("viewPos", World::getInstance().getActiveCamera()->getTransform()->getPosition());
+		shader->SetFloat3("ambientLight", World::getInstance().getActiveCamera()->getAmbientLight());
+
+		for(unsigned int i = 0; i < directionalLights.size() && i < 4; ++i)
+		{
+			DirectionalLight* directionalLight = directionalLights[i];
+
+
+			shader->SetFloat3("directionalLights[" + std::to_string(i) + "].base.color", directionalLight->getColor());
+			shader->SetFloat("directionalLights[" + std::to_string(i) + "].base.intensity", directionalLight->getIntensity());
+			shader->SetFloat3("directionalLights[" + std::to_string(i) + "].direction", directionalLight->getTransform()->getForwardVector());
+		}
+
+		for (unsigned int i = 0; i < pointLights.size() && i < 4; ++i)
+		{
+			PointLight* pointLight = pointLights[i];
+
+			shader->SetFloat3("pointLights[" + std::to_string(i) + "].base.color", pointLight->getColor());
+			shader->SetFloat("pointLights[" + std::to_string(i) + "].base.intensity", pointLight->getIntensity());
+			shader->SetFloat3("pointLights[" + std::to_string(i) + "].position", pointLight->getTransform()->getPosition());
+			shader->SetFloat("pointLights[" + std::to_string(i) + "].attenuation.constant", pointLight->getAttenuation().constant);
+			shader->SetFloat("pointLights[" + std::to_string(i) + "].attenuation.linear", pointLight->getAttenuation().linear);
+			shader->SetFloat("pointLights[" + std::to_string(i) + "].attenuation.exponential", pointLight->getAttenuation().exponential);
+			shader->SetFloat("pointLights[" + std::to_string(i) + "].radius", pointLight->getRadius());
+		}
+
+		for (unsigned int i = 0; i < spotLights.size() && i < 4; ++i)
+		{
+			SpotLight* spotLight = spotLights[i];
+
+			shader->SetFloat3("spotLights[" + std::to_string(i) + "].pointLight.base.color", spotLight->getColor());
+			shader->SetFloat("spotLights[" + std::to_string(i) + "].pointLight.base.intensity", spotLight->getIntensity());
+			shader->SetFloat3("spotLights[" + std::to_string(i) + "].pointLight.position", spotLight->getTransform()->getPosition());
+			shader->SetFloat("spotLights[" + std::to_string(i) + "].pointLight.attenuation.constant", spotLight->getAttenuation().constant);
+			shader->SetFloat("spotLights[" + std::to_string(i) + "].pointLight.attenuation.linear", spotLight->getAttenuation().linear);
+			shader->SetFloat("spotLights[" + std::to_string(i) + "].pointLight.attenuation.exponential", spotLight->getAttenuation().exponential);
+			shader->SetFloat("spotLights[" + std::to_string(i) + "].pointLight.radius", spotLight->getRadius());
+			shader->SetFloat3("spotLights[" + std::to_string(i) + "].direction", spotLight->getTransform()->getForwardVector());
+			shader->SetFloat("spotLights[" + std::to_string(i) + "].cutoff", spotLight->getCutOff());
+		}
 
 		for (unsigned int i = 0; i < terrains.size(); ++i)
 		{
